e9080d94e37cd215f8d4aeb15bd7ca151c8944cd
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-05-09 20:29:52 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,17 @@
-export const App = () => {
-  return <div></div>;
-};
+import { Component } from 'react';
+import { Searchbar } from './Searchbar';
+import { Container } from './App.styled';
+
+export class App extends Component {
+  formSubmitHandler = formData => {
+    console.log(formData);
+  };
+
+  render() {
+    return (
+      <Container>
+        <Searchbar onSubmit={this.formSubmitHandler} />
+      </Container>
+    );
+  }
+}
