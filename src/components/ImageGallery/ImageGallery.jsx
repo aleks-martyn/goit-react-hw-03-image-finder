e1f583102f9bac40e2fb2664f514961e38254eb3
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-05-11 20:22:39 +0300

    Update ImageGallery.jsx

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,21 +1,31 @@
-import PropTypes from 'prop-types';
-import { GalleryItem } from './ImageGalleryItem';
+import { Component } from 'react';
+//import { GalleryItem } from 'components/ImageGalleryItem';
 import { GalleryList } from './ImageGallery.styled';
 
-export const Gallery = ({ images }) => {
-  return (
-    <GalleryList>
-      {images.map(({ id, preview, description }) => (
-        <GalleryItem key={id} src={preview} alt={description} />
-      ))}
-    </GalleryList>
-  );
-};
+export class Gallery extends Component {
+  state = {
+    response: '',
+    status: null,
+  };
 
-GalleryList.propTypes = {
-  images: PropTypes.arrayOf(
-    PropTypes.shape({
-      id: PropTypes.number.isRequired,
-    })
-  ),
-};
+  componentDidUpdate(prevProps, prevState) {
+    const prevSearchQuery = prevProps.searchQuery;
+    const nextSearchQuery = this.props.searchQuery;
+
+    if (prevSearchQuery !== nextSearchQuery) {
+      this.setState({ status: 'pending' });
+      fetch(
+        `https://pixabay.com/api/?key=34753059-f7902d1f02de9c533025c1a5e&q=${nextSearchQuery}&image_type=photo`
+      );
+    }
+  }
+
+  render() {
+    const { response, status } = this.state;
+    return (
+      <GalleryList>
+        <p>{this.props.searchQuery}</p>
+      </GalleryList>
+    );
+  }
+}
