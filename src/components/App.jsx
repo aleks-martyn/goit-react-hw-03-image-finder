6f90d9ac7a3f7a71d00daa3a71d147b65d81c084
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-05-11 20:19:34 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,6 @@
 import { Component } from 'react';
 import { Searchbar } from './Searchbar';
+import { Gallery } from './ImageGallery';
 import { Container } from './App.styled';
 
 export class App extends Component {
@@ -8,13 +9,14 @@ export class App extends Component {
   };
 
   formSubmitHandler = searchQuery => {
-    this.setState({searchQuery});
+    this.setState({ searchQuery });
   };
 
   render() {
     return (
       <Container>
         <Searchbar onSubmit={this.formSubmitHandler} />
+        <Gallery searchQuery={this.state.searchQuery} />
       </Container>
     );
   }
