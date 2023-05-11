c8696dfc2c6163496fd25398cb5a76fa5eb2b6d7
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-05-11 16:08:37 +0300

    Update ImageGalleryItem.jsx

--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -1,15 +1,15 @@
 import PropTypes from 'prop-types';
 import { Item, Image } from './ImageGalleryItem.styled';
 
-export const GalleryItem = ({ url, description }) => {
+export const GalleryItem = ({ preview, description }) => {
   return (
     <Item>
-      <Image src={url} alt={description} />
+      <Image src={preview} alt={description} />
     </Item>
   );
 };
 
 GalleryItem.propTypes = {
-  url: PropTypes.string.isRequired,
+  preview: PropTypes.string.isRequired,
   description: PropTypes.string.isRequired,
 };
