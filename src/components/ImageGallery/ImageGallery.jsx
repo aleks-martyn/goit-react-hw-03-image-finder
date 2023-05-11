101dcabc713ce5a0e4b7c87840ee165337343ab8
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-05-12 00:46:55 +0300

    Update ImageGallery.jsx

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -5,6 +5,7 @@ import { GalleryList } from './ImageGallery.styled';
 export class Gallery extends Component {
   state = {
     data: {},
+    loading: false,
   };
 
   componentDidUpdate(prevProps, prevState) {
@@ -12,19 +13,25 @@ export class Gallery extends Component {
     const nextSearchQuery = this.props.searchQuery;
 
     if (prevSearchQuery !== nextSearchQuery) {
+      this.setState({ loading: true });
       fetch(
         `https://pixabay.com/api/?key=34753059-f7902d1f02de9c533025c1a5e&q=${nextSearchQuery}&image_type=photo`
       )
         .then(res => res.json())
-        .then(data => this.setState({ data }));
+        .then(data => this.setState({ data }))
+        .finally(() => this.setState({ loading: false }));
     }
   }
 
   render() {
-    const { data: {hits} } = this.state;
+    const {
+      data: { hits },
+    } = this.state;
 
     return (
       <GalleryList>
+        {this.state.loading && <div>Йде запит...</div>}
+        {!this.props.searchQuery && <div>Введіть пошуковий запит.</div>}
         {hits &&
           hits.map(({ id, webformatURL, tags }) => (
             <GalleryItem key={id} webformatURL={webformatURL} tags={tags} />
