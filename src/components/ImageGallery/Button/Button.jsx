2aa13a3b792f90c79543bc4d2ebca367878d2b3b
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-05-12 13:18:55 +0300

    Update Button.jsx

--- src/components/ImageGallery/Button/Button.jsx
+++ src/components/ImageGallery/Button/Button.jsx
@@ -2,7 +2,9 @@ import PropTypes from 'prop-types';
 import { Button } from './Button.styled';
 
 export const LoadMoreBtn = ({ onClick }) => (
-  <Button type="button">Load more</Button>
+  <Button type="button" onClick={onClick}>
+    Load more
+  </Button>
 );
 
 LoadMoreBtn.propTypes = {
