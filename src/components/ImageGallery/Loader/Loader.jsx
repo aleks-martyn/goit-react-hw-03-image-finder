cdabedeff0454409a752e7d350d95d5f297df12e
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-05-12 16:50:04 +0300

    Update Loader.jsx

--- src/components/ImageGallery/Loader/Loader.jsx
+++ src/components/ImageGallery/Loader/Loader.jsx
@@ -1,11 +1,14 @@
 import { RotatingLines } from 'react-loader-spinner';
+import { Wrap } from './Loader.styled';
 
 export const Spinner = () => (
-  <RotatingLines
-    strokeColor="grey"
-    strokeWidth="5"
-    animationDuration="0.75"
-    width="96"
-    visible={true}
-  />
+  <Wrap>
+    <RotatingLines
+      strokeColor="grey"
+      strokeWidth="5"
+      animationDuration="0.75"
+      width="96"
+      visible={true}
+    />
+  </Wrap>
 );
