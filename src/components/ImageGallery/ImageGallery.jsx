a18b86bc7a89864bb2c0c510a2be45d623a03568
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-05-13 01:25:38 +0300

    Update ImageGallery.jsx

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import { GalleryItem } from './ImageGalleryItem';
 import { LoadMoreBtn } from './Button';
 import { Spinner } from './Loader';
+import { Modal } from './Modal';
 import imagesAPI from './services/images-api';
 import { Wrap, GalleryList } from './ImageGallery.styled';
 
@@ -11,7 +12,16 @@ export class Gallery extends Component {
     data: {},
     error: null,
     status: 'idle',
+    showModal: false,
+    largeImageURL: "",
+    tags: "",
   };
+
+  //componentDidMount() {
+  //  window.addEventListener('click', event => {
+  //    console.log(event.target)
+  //  });
+  //}
 
   componentDidUpdate(prevProps, prevState) {
     const prevSearchQuery = prevProps.searchQuery;
@@ -27,6 +37,11 @@ export class Gallery extends Component {
     }
   }
 
+  toggleModal = (largeImageURL, tags) => {
+    console.log(largeImageURL);
+    this.setState(({ showModal, largeImageURL, tags }) => ({ showModal: !showModal, largeImageURL, tags }));
+  };
+
   btnClickHandler = event => {
     console.log(event);
   };
@@ -36,8 +51,11 @@ export class Gallery extends Component {
       data: { hits },
       error,
       status,
+      showModal,
+      largeImageURL,
+      tags,
     } = this.state;
-
+    console.log(largeImageURL);
     if (status === 'idle') {
       return <div>Введіть пошуковий запит.</div>;
     }
@@ -55,11 +73,25 @@ export class Gallery extends Component {
         <Wrap>
           <GalleryList>
             {hits.length === 0 && <p>За цим запитом нічого не знайдено.</p>}
-            {hits.map(({ id, webformatURL, tags }) => (
-              <GalleryItem key={id} webformatURL={webformatURL} tags={tags} />
+            {hits.map(({ id, webformatURL, tags, largeImageURL }) => (
+              <GalleryItem
+                key={id}
+                webformatURL={webformatURL}
+                tags={tags}
+                largeImageURL={largeImageURL}
+                openModal={this.toggleModal}
+              />
             ))}
           </GalleryList>
           {hits.length > 0 && <LoadMoreBtn onClick={this.btnClickHandler} />}
+          {showModal && (
+            <Modal>
+              <img
+                src={largeImageURL}
+                alt={tags}
+              />
+            </Modal>
+          )}
         </Wrap>
       );
     }
