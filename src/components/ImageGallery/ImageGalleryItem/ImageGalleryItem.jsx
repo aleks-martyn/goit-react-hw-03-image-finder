0298621ba20e6a415bee72b037494f97fa37228e
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-05-13 01:25:25 +0300

    Update ImageGalleryItem.jsx

--- src/components/ImageGallery/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGallery/ImageGalleryItem/ImageGalleryItem.jsx
@@ -1,13 +1,27 @@
 import PropTypes from 'prop-types';
 import { Item, Image } from './ImageGalleryItem.styled';
 
-export const GalleryItem = ({ webformatURL, tags }) => (
-  <Item>
-    <Image src={webformatURL} alt={tags} />
-  </Item>
-);
+export const GalleryItem = ({
+  webformatURL,
+  tags,
+  largeImageURL,
+  openModal,
+}) => {
+  console.log(largeImageURL);
+  return (
+    <Item>
+      <Image
+        src={webformatURL}
+        alt={tags}
+        onClick={() => openModal(largeImageURL, tags)}
+      />
+    </Item>
+  );
+};
 
 GalleryItem.propTypes = {
   webformatURL: PropTypes.string.isRequired,
   tags: PropTypes.string.isRequired,
+  largeImageURL: PropTypes.string.isRequired,
+  openModal: PropTypes.func.isRequired,
 };
