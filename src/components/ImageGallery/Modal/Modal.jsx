3b1833e2e732b83ee8f7f39d25d53c63c5bf3d98
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-05-13 10:47:05 +0300

    Update Modal.jsx

--- src/components/ImageGallery/Modal/Modal.jsx
+++ src/components/ImageGallery/Modal/Modal.jsx
@@ -1,21 +1,38 @@
 import { Component } from 'react';
-//import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 import { Overlay, ModalWin } from './Modal.styled';
 
 export class Modal extends Component {
-  //componentDidMount() {
-  //  console.log('Modal componentDidMount');
-  //}
+  componentDidMount() {
+    window.addEventListener('keydown', this.handleKeyDown);
+  }
 
-  //componentWillUnmount() {
-  //  console.log('Modal componentWillUnmount');
-  //}
+  componentWillUnmount() {
+    window.removeEventListener('keydown', this.handleKeyDown);
+  }
+
+  handleKeyDown = event => {
+    if (event.code === 'Escape') {
+      this.props.onClose();
+    }
+  };
+
+  handleOverlayClick = event => {
+    if (event.currentTarget === event.target) {
+      this.props.onClose();
+    }
+  };
 
   render() {
     return (
-      <Overlay>
+      <Overlay onClick={this.handleOverlayClick}>
         <ModalWin>{this.props.children}</ModalWin>
       </Overlay>
     );
   }
 }
+
+Modal.propTypes = {
+  onClose: PropTypes.func.isRequired,
+  children: PropTypes.node,
+};
