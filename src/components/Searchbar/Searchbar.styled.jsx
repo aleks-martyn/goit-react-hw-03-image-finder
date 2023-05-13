d0a220d662fc0f5fd67cf70a24b3683f15f5cc9a
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-05-13 15:08:27 +0300

    Update Searchbar.styled.jsx

--- src/components/Searchbar/Searchbar.styled.jsx
+++ src/components/Searchbar/Searchbar.styled.jsx
@@ -48,18 +48,6 @@ export const SearchButton = styled.button`
   }
 `;
 
-export const ButtonLabel = styled.span`
-  position: absolute;
-  width: 1px;
-  height: 1px;
-  padding: 0;
-  overflow: hidden;
-  clip: rect(0, 0, 0, 0);
-  white-space: nowrap;
-  clip-path: inset(50%);
-  border: 0;
-`;
-
 export const Input = styled(Field)`
   display: inline-block;
   width: 100%;
