2c48e1f3ecff3d3b503790f741706c48c4599dd2
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-05-14 10:26:57 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,22 +1,67 @@
 import { Component } from 'react';
 import { Searchbar } from './Searchbar';
 import { Gallery } from './ImageGallery';
+import imagesAPI from './services/images-api';
+import { LoadMoreBtn } from './Button';
+import { Spinner } from './Loader';
 import { Container } from './App.styled';
 
 export class App extends Component {
   state = {
     searchQuery: '',
+    page: 1,
+    hits: [],
+    error: null,
+    status: 'idle',
   };
 
+  componentDidUpdate(prevProps, prevState) {
+    const prevSearchQuery = prevState.searchQuery;
+    const nextSearchQuery = this.state.searchQuery;
+    const prevPage = prevState.page;
+    const nextPage = this.state.page;
+
+    if (prevSearchQuery !== nextSearchQuery || prevPage !== nextPage) {
+      this.setState({ status: 'pending' });
+
+      imagesAPI
+        .fetchImages(nextSearchQuery, nextPage)
+        .then(({ hits }) =>
+          this.setState(prevState => ({
+            hits: [...prevState.hits, ...hits],
+            status: 'resolved',
+          }))
+        )
+        .catch(error => this.setState({ error, status: 'rejected' }));
+    }
+  }
+
   formSubmitHandler = searchQuery => {
-    this.setState({ searchQuery });
+    this.setState({
+      searchQuery,
+      page: 1,
+      error: null,
+      hits: [],
+      status: 'idle',
+    });
+  };
+
+  btnClickHandler = () => {
+    this.setState(({ page }) => ({ page: page + 1 }));
   };
 
   render() {
+    const { hits, status, error } = this.state;
+
     return (
       <Container>
         <Searchbar onSubmit={this.formSubmitHandler} />
-        <Gallery searchQuery={this.state.searchQuery} />
+        {status === 'idle' && <h2>Enter a search query.</h2>}
+        {status === 'pending' && <Spinner />}
+        <Gallery hits={hits} />
+        {hits && hits.length > 0 && (
+          <LoadMoreBtn onClick={this.btnClickHandler} />
+        )}
       </Container>
     );
   }
